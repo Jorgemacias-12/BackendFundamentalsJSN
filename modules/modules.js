702d240856f3eb3794e3ce9db1009dfe21c0f169
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-04-09 01:53:48 -0500

    Terminado codigo de ejemplo #2
    	Agregando codigo para la seccion de modulos
    	closes #2

---
+++ modules/modules.js
@@ -0,0 +1,8 @@
+// Modulos
+
+const names = require('./names');
+
+names.saludar(names.Jorge);
+names.saludar(names.Aaron);
+names.saludar(names.Emilio);
+names.saludar(names.Jose);
---
+++ modules/names.js
@@ -0,0 +1,19 @@
+const Jorge = "Jorge";
+const Aaron = "Aaron";
+const Jose = "Jose";
+const Emilio = "Emilio";
+
+/*
+  Local variable that stores our API KEY from digital ocean :D
+*/
+const API_KEY = "014906CA93C9C7B684696218B7E0C4C2BF50EF4A";
+
+const saludar = (nombre) => {
+  console.log(`Hola ${nombre}`)
+}
+
+
+module.exports = {
+  Jorge, Aaron, Jose, Emilio, saludar
+};
+
